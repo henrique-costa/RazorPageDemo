27c36aa4354d8d78cb7bcf528b6fd9959bb6265c
Henrique Costa <henrique_vc@hotmail.com>
2022-09-12 04:03:08 -0300

    Add project files.

---
+++ RazorPagesUI/Model/PropostaRecebida.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesUI.Model
+{
+    public class PropostaRecebida
+    {
+        [Required]
+        public string JsonPropost { get; set; }
+        public int versaoPropost { get; set; }
+        public string tituloPropost { get; set; }
+    }
+}
---
+++ RazorPagesUI/Pages/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RazorPagesUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public PropostaRecebida Proposta { get; set; }
+
+        [BindProperty]
+        public string JsonPropost { get; set; }
+        [BindProperty]
+        public int versaoPropost { get; set; }
+        [BindProperty]
+        public string tituloPropost { get; set; }
+
+       
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string JsonPropost, int versaoPropost, string tituloPropost)
+        {
+            ViewData["confirmation"] = $"{JsonPropost}, {versaoPropost}, {tituloPropost}";
+
+            Console.WriteLine($"Passou {JsonPropost}, {versaoPropost}, {tituloPropost}");
+        }
+
+        public async Task<IActionResult> OnPostRegisterAsync()
+        {
+
+                       //…
+            return RedirectToPage("/Privacy");
+        }
+        public async Task<IActionResult> OnPostRequestInfo()
+        {
+            //…
+            return RedirectToPage();
+        }
+    }
+}
